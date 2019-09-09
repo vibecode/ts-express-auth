145d540d10e4d439b3fbcbec2afd51aba02ba9ea
vibecode <rumashkin@gmail.com>
2019-09-09 14:08:10 +0200

    setup express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log('hi')
+import express, { Request, Response } from 'express'
 
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`<h1>Hi</h1>`)
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
