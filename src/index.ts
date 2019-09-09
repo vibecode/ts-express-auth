2cd61bf199f8755f9bb1fb9580443638a6e49397
vibecode <rumashkin@gmail.com>
2019-09-09 19:31:39 +0200

    implement routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express'
+import { router } from './routes'
+import bodyParser from 'body-parser'import cookieSession from 'cookie-session'
+
+
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`<h1>Hi</h1>`)
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({ keys: ['qdoritfdsdgeeesdfls']}))
+app.use(router)
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
---
+++ src/routes.ts
@@ -0,0 +1,69 @@
+import { Router, Request, Response, NextFunction } from 'express'
+import { request } from 'https'
+
+const router = Router()
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined }
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next()
+    return
+  }
+
+  res.status(403)
+  res.send('Access denied')
+}
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+  <form method="Post">
+    <div>
+        <label>Email</label>
+        <input name="email"/>
+    </div>
+
+    <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+    </div>
+    <button>Submit</button>
+  </form> `)
+})
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body
+
+  if (email && password && email === 'test' && password === 'test') {
+    req.session = { loggedIn: true }
+    res.redirect('/')
+  } else {
+    res.send('Invalid email or password')
+  }
+})
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Log out</a>
+      </div>
+    `)
+  } else {
+    res.send('')
+  }
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user')
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined
+  res.redirect('/')
+})
+
+export { router }
